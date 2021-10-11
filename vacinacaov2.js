5640a9646ce9ac16f287d71d2cdb113b7a238c1f
tiagopestanaa <92305816+tiagopestanaa@users.noreply.github.com>
2021-10-11 12:42:36 +0100

    Add files via upload

---
+++ vacinacaov2.js
@@ -0,0 +1,10 @@
+const nome = "VacinacaoCovid19"; // Declarar uma constante e atribuir-lhe o valor "VacinacaoCovid19" (um 'string', uma cadeia de carateres)
+
+ // Definir tipos (classes) de objetos que a app gerirá ("type of objects")
+class Utente {}
+
+class Vacina {}
+
+class Centro_Vacinacao {}
+
+class Vacinacao {}
